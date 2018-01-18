8d927634b6a6c0aff0037fa7bf0ad00f8a863a36
jenia1992 <33311012+jenia1992@users.noreply.github.com>
2018-01-18 21:54:39 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+$(function(){ //like main in C
+        $(window).keypress(function(e){
+            if(e.keyCode ===13){
+                newItem();  
+            }
+        });
+
+        function newItem(){
+            var item=$('#todo').val();
+            
+             if(item!='')$('#todos').append('<li>'+ item +'</li>');
+            $('#todo').val('');
+        }
+
+        $('ul').on('click','li',function(e){
+            $(e.target).remove();
+        });
+
+
+
+
+    });
